2c3119a7cc7da6c23f4431cdb2079ae8ca736f99
David <khyu777@gmail.com>
2018-06-01 13:27:03 -0400

    use .format

--- rename.py
+++ rename.py
@@ -2,24 +2,26 @@ import os
 from os.path import isfile, join
 import datetime
 
+folder = input('Folder path: ')
+path = 'C:/Users/David/Documents/Python/' + folder
+os.chdir(path)
+print(os.getcwd())
+
 files = [f for f in os.listdir() if isfile(join(f))]
-newname = []
-bikeID = newname.append(input('BikeID: '))
-device = newname.append(input('Device: '))
-serial = newname.append(input('Last 4 serial #: '))
+bikeID = input('BikeID: ')
+device = input('Device: ')
+serial = input('Last 4 serial #: ')
+date = ''
 while True:
     samedate = input('Using today\'s date? (Y/N)')
     if samedate.lower() == 'y':
-        date = newname.append(datetime.datetime.today().strftime('%y%m%d'))
-        break
+        date = datetime.datetime.today().strftime('%y%m%d')
     elif samedate.lower() == 'n':
-        date = newname.append(input('Date: '))
-        break
-ext = input('extension: ')
-for file in range(len(files)):
-    print('\n'+files[file])
+        date = input('Date: ')
+    break
+ext = input('Extension: ')
+for f in range(len(files)):
+    print('\n'+files[f])
     session = input('Session: ')
-    newname.insert(3, 'S' + session)
-    filename = '_'.join(newname) + '.' + ext
-    os.rename(files[file], filename)
-    del newname[3]
+    filename = '{}_{}_{}_S{}_{}.{}'.format(bikeID, device, serial, session, date, ext)
+    os.rename(files[f], filename)
